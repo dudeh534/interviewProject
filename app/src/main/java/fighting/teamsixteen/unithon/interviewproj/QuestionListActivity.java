1a24f7c9f8670ac7dadd08b884960d9126a4801c
Youngdo <youngdo9647@gmail.com>
2016-02-13 22:36:32 +0900

    qus

--- app/src/main/java/fighting/teamsixteen/unithon/interviewproj/QuestionListActivity.java
+++ app/src/main/java/fighting/teamsixteen/unithon/interviewproj/QuestionListActivity.java
@@ -1,11 +1,7 @@
 package fighting.teamsixteen.unithon.interviewproj;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.Button;
 
 public class QuestionListActivity extends AppCompatActivity {
 
@@ -13,14 +9,6 @@ public class QuestionListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_list);
-        Button goNext = (Button)findViewById(R.id.btn_go_videolist);
-        goNext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                Intent i = new Intent(QuestionListActivity.this, VideoListActivity.class);
-                startActivity(i);
-            }
-        });
 
     }
 
