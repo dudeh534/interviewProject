e762081741919880a5f698f1e2bd71b3400eef8a
ChaeYoungDo <dudeh534@naver.com>
2016-02-13 11:48:12 +0900

    splash

---
+++ app/src/main/java/fighting/teamsixteen/unithon/interviewproj/SplashActivity.java
@@ -0,0 +1,27 @@
+package fighting.teamsixteen.unithon.interviewproj;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Handler handler = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                Intent intent = new Intent(SplashActivity.this, GroupListActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        handler.sendEmptyMessageDelayed(0, 3000);
+
+    }
+}
