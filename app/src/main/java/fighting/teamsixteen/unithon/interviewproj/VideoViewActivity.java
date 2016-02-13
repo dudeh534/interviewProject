87d18a6819c52906b803be88b2fe8b360afc236d
fennox <dlagkrbs8855@naver.com>
2016-02-13 23:00:28 +0900

    videomake, videoview  upload

--- app/src/main/java/fighting/teamsixteen/unithon/interviewproj/VideoViewActivity.java
+++ app/src/main/java/fighting/teamsixteen/unithon/interviewproj/VideoViewActivity.java
@@ -1,17 +1,58 @@
 package fighting.teamsixteen.unithon.interviewproj;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.TextView;
+import android.widget.VideoView;
 
 public class VideoViewActivity extends AppCompatActivity {
+
+    VideoView videoview;
+    TextView testTv;
+    ImageView btn_addmemo;
+
+
+    MediaController mediaController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_view);
 
+        mediaController = new MediaController(this);
+
+        videoview = (VideoView) findViewById(R.id.videoview);
+        testTv = (TextView) findViewById(R.id.testTv);
+        btn_addmemo = (ImageView) findViewById(R.id.btn_addmemo);
+        btn_addmemo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                videoview.pause();
+                String tmp = testTv.getText().toString() + "\n" + videoview.getCurrentPosition() / 1000;
+                testTv.setText(tmp);
+                mediaController.show();
+            }
+        });
+
+        //videoview.setVideoPath();
+        String PATH = "mnt/sdcard/videooutput.mp4";
+        videoview.setVideoPath(PATH);
+//        videoview.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.aaaa));
+
+//        mediaController.setAnchorView(videoview);
+        videoview.setMediaController(mediaController);
+        videoview.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                videoview.start();
+                testTv.setText("\n비디오 총시간 : " + videoview.getDuration() / 1000);//비디오 총시간?
+            }
+        });
     }
 
 }
