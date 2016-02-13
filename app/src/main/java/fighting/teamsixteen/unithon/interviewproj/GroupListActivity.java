40f04efc718ed463284559263b186fe4ff987503
ChaeYoungDo <dudeh534@naver.com>
2016-02-13 15:28:16 +0900

    recyclerView

--- app/src/main/java/fighting/teamsixteen/unithon/interviewproj/GroupListActivity.java
+++ app/src/main/java/fighting/teamsixteen/unithon/interviewproj/GroupListActivity.java
@@ -1,19 +1,17 @@
 package fighting.teamsixteen.unithon.interviewproj;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.widget.Button;
+import android.widget.FrameLayout;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class GroupListActivity extends AppCompatActivity {
-
+        private FrameLayout fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,27 +20,20 @@ public class GroupListActivity extends AppCompatActivity {
         LinearLayoutManager layoutManager=new LinearLayoutManager(getApplicationContext());
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(layoutManager);
-
+        fab = (FrameLayout)findViewById(R.id.fab);
         List<Recycler_item> items=new ArrayList<>();
         Recycler_item[] item=new Recycler_item[5];
-        item[0]=new Recycler_item(R.drawable.a,"#1");
-        item[1]=new Recycler_item(R.drawable.a,"#2");
-        item[2]=new Recycler_item(R.drawable.a,"#3");
-        item[3]=new Recycler_item(R.drawable.a,"#4");
-        item[4]=new Recycler_item(R.drawable.a,"#5");
+        item[0]=new Recycler_item(R.drawable.button_floating,"인턴면접");
+        item[1]=new Recycler_item(R.drawable.button_floating,"동아리면접");
+        item[2]=new Recycler_item(R.drawable.button_floating,"개인면접");
+        item[3]=new Recycler_item(R.drawable.button_floating,"유니톤면접");
+        item[4]=new Recycler_item(R.drawable.button_floating,"공채면접");
 
         for(int i=0;i<5;i++) items.add(item[i]);
 
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
-        recyclerView.setAdapter(new RecyclerAdapter(getApplicationContext(),items,R.layout.activity_main));
-        Button goNext = (Button)findViewById(R.id.btn_go_question);
-        goNext.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View arg0) {
-                Intent i = new Intent(GroupListActivity.this, QuestionListActivity.class);
-                startActivity(i);
-            }
-        });
+        recyclerView.setAdapter(new RecyclerAdapter(getApplicationContext(), items, R.layout.activity_main));
+
     }
 
 }
