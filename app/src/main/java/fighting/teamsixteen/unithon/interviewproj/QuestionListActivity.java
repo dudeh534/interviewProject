c349807b7770f057892eca79864814d51d0bebd8
ChaeYoungDo <dudeh534@naver.com>
2016-02-14 05:52:25 +0900

    last

--- app/src/main/java/fighting/teamsixteen/unithon/interviewproj/QuestionListActivity.java
+++ app/src/main/java/fighting/teamsixteen/unithon/interviewproj/QuestionListActivity.java
@@ -7,7 +7,6 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.Button;
 import android.widget.FrameLayout;
 
 import java.util.ArrayList;
@@ -38,7 +37,7 @@ public class QuestionListActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         final List<Recycler_Item_question> items = new ArrayList<>();
         final Recycler_Item_question[] item = new Recycler_Item_question[20];
-<<<<<<< HEAD
+
         item[0] = new Recycler_Item_question("자기 소개 5분동안 하기","화이팅","1");
         item[1] = new Recycler_Item_question("자기 소개 5분동안 하기","화이팅","2");
         item[2] = new Recycler_Item_question("자기 소개 5분동안 하기","화이팅","3");
@@ -47,28 +46,6 @@ public class QuestionListActivity extends AppCompatActivity {
         recyclerView.setAdapter(new RecyclerAdapter_question(getApplicationContext(), items, R.layout.activity_question_list));
         fab = (FrameLayout) findViewById(R.id.fab1);
         fab.setOnClickListener(new View.OnClickListener() {
-=======
-        if(questions[0] != null) {
-            for (int i = 0; i < questions.length; i++) {
-                String formatStr = "해당 질문에 대한 답변이 " + questions[i].getAnswerCount() + "개 있습니다.";
-                item[i] = new Recycler_Item_question(questions[i].getQuestion(), formatStr, "" + (i + 1));
-            }
-            for (int i = 0; i < questions.length; i++) items.add(item[i]);
-            recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
-        }
-            recyclerView.setAdapter(new RecyclerAdapter_question(getApplicationContext(), items, R.layout.activity_question_list));
-            fab = (FrameLayout) findViewById(R.id.fab1);
-            fab.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(QuestionListActivity.this, Question_Register.class);
-                    startActivity(intent);
-                }
-            });
-
-        Button back = (Button) findViewById(R.id.information_btn_toggle122);
-        back.setOnClickListener(new View.OnClickListener() {
->>>>>>> 98d8f25624ad4398e7eb64c8617b721c5fa9d64a
             @Override
             public void onClick(View v) {
                 finish();
--- app/src/main/java/fighting/teamsixteen/unithon/interviewproj/Question_Register.java
+++ app/src/main/java/fighting/teamsixteen/unithon/interviewproj/Question_Register.java
@@ -4,11 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-<<<<<<< HEAD
 import android.widget.ImageView;
-=======
-import android.widget.Button;
->>>>>>> 98d8f25624ad4398e7eb64c8617b721c5fa9d64a
 
 /**
  * Created by ChaeYoungDo on 2016-02-14.
@@ -19,20 +15,13 @@ public class Question_Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.question_register);
-<<<<<<< HEAD
         recording=(ImageView)findViewById(R.id.recording);
         recording.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i=new Intent(Question_Register.this,RecordActivity.class);
                 startActivity(i);
-=======
-        Button button = (Button) findViewById(R.id.information_btn_toggle12);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
->>>>>>> 98d8f25624ad4398e7eb64c8617b721c5fa9d64a
+
             }
         });
     }
